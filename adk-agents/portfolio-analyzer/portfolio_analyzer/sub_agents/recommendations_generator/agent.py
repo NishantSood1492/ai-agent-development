01b05e683a4a3818da43397e12b0b1dca9a22a8f
Nishant Sood <nishantsood@duck.com>
2025-07-29 15:24:51 -0400

    updated recommendations generator to use model name from constants

--- adk-agents/portfolio-analyzer/portfolio_analyzer/sub_agents/recommendations_generator/agent.py
+++ adk-agents/portfolio-analyzer/portfolio_analyzer/sub_agents/recommendations_generator/agent.py
@@ -2,12 +2,11 @@ from google.adk import Agent
 
 from . import prompt
 from . import tools
-
-MODEL = "gemini-2.5-flash"
+from ... import constants
 
 
 recommendations_generator_agent = Agent(
-    model=MODEL,
+    model=constants.GEMINI_MODEL_NAME,
     name="recommendations_generator_agent",
     description="Generates top 3 recommendations based on {cash_drag_output} and {diversification_scorer_output}",
     instruction=prompt.RECOOMENDATIONS_GENERATOR_PROMPT,
